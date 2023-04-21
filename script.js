a7cc65ae889fca68289926166b72b9968e406580
Murat351 <murat351@narod.ru>
2023-04-21 16:34:33 +0400

    problem this.next

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ let abonent={
 	ask:0,
 	find:0,
 };
-let controllers={};  //список адресов найденных контроллеров
+let controllers={};  //список объектов найденных контроллеров по сетевым адресам
+let cards={};  //список объектов карт по номерам карт
 let aa; //= new MyClass(abonent.writer); //параметр - функция отправки сообщения
 let t_start;  //отладочная переменная для измерения времени ответа
 
@@ -78,27 +79,85 @@ function *gen(data, writer, comment){ //команда с переходом п�
 		txt = control.buff_sum([txt, answer]);
 		if(txt.length>=13){
 			clearTimeout(timer);
-			let timer1=setTimeout(()=>{control[writer]( txt, comment); this.next();}, 5)
+			if(data[0]){
+				if(!(data[0] in controllers)){
+					controllers[data[0]]={address:data[0]};
+					controllers[data[0]].open_door=return_null();//заглушка
+				}
+			}
+			let timer1=setTimeout(()=>{ this.next();}, 5)
 		}
 		answer = yield;
 	}
 }
 
-function *skud(data, comment){ //команда обработки событий СКУД
-	//let txt=new Uint8Array(0);
-	//data[0]
-	// --------------------------тут нужна логика обработки ответа
-	//this.writer.write(data); //отправляем подготовленное сообщение
-	//let timer=setTimeout(()=>{control[writer]( txt, comment); this.next();}, 35);
-	//let answer = yield; //получаем ответ от устройства
-	//while(true) {
-	//	txt = control.buff_sum([txt, answer]);
-	//	if(txt.length>=13){
-	//		clearTimeout(timer);
-	//		let timer1=setTimeout(()=>{control[writer]( txt, comment); this.next();}, 5)
-	//	}
-	//	answer = yield;
-	//}
+
+function *scan(data, writer, comment){ //команда с переходом по команде вложенной функции
+	let txt=new Uint8Array(0);
+	this.writer.write(data); //отправляем подготовленное сообщение
+	let timer=setTimeout(()=>{this.next();}, 110);
+	let answer = yield; //получаем ответ от устройства
+	while(true) {
+		txt = control.buff_sum([txt, answer]);
+		if(txt.length>=13){
+			clearTimeout(timer);
+			txt=new Uint8Array(0);
+			//control[writer]( answer, comment);
+			console.log(writer+" open_door -1");
+			if(answer[4]&0x1F){//ТМ
+				//по адресу находим объект контроллера
+				//в этот объект вставляем экземпляр генератора open_door
+				//если есть карта, добавляем ее номер в объект cards
+				console.log("new skud");
+				if(answer[0] in controllers){
+					//controllers[answer[0]].open_door=open_door();
+					//controllers[answer[0]].open_door.next();
+					yield* open_door (answer, comment);
+				}
+			}else{
+				console.log("skud 2");
+				//controllers[answer[0]].open_door.next();
+				yield* return_null (answer, comment)
+			}
+		}
+		answer = yield;
+	}
+}
+
+function *open_door(data, comment){ //команда обработки событий СКУД
+		//выводим сообщение
+		pole.innerText=data+"\r\n"+(+new Date()-t_start);
+		t_start=+new Date();
+		let adr = new Uint8Array([data[0], 0x06, 0x00, 0x04, 0x00, 0xFF]); //открыть замок
+		let msg = control.buff_sum([adr, getCRC(adr)]);//расчитать CRC
+		let timer3=setTimeout(()=>{this.writer.write(msg);}, 5)
+		console.log("open_door 1");
+		yield 1;
+		pole.innerText=data+"\r\n"+(+new Date()-t_start);
+		t_start=+new Date();
+		adr = new Uint8Array([data[0], 0x06, 0x00, 0x01, 0x00, 0xFF]); //зеленый светодиод
+		msg = control.buff_sum([adr, getCRC(adr)]);//расчитать CRC
+		timer3=setTimeout(()=>{this.writer.write(msg);}, 5)
+		console.log("open_door 2");
+		yield 2;
+		pole.innerText=data+"\r\n"+(+new Date()-t_start);
+		t_start=+new Date();
+		adr = new Uint8Array([data[0], 0x06, 0x00, 0x03, 0x00, 0xFF]); //звук
+		msg = control.buff_sum([adr, getCRC(adr)]);//расчитать CRC
+		timer3=setTimeout(()=>{this.writer.write(msg);}, 5)
+		console.log("open_door 0");
+		//yield 2;
+		this.next();
+		yield 0;
+}
+function *return_null(data, comment){ //функция заглушка
+	while(true) {
+		//выводим сообщение
+		pole.innerText=data+"\r\n"+(+new Date()-t_start);
+		t_start=+new Date();
+		this.next();
+		yield 0;
+	}
 }
 
 
@@ -159,7 +218,7 @@ let control={
 		for (let value in controllers) {
 			let adr = new Uint8Array([value, 0x04, 0x00, 0x00, 0x00, 0x05]); //опрос первого адреса
 			let data = control.buff_sum([adr, getCRC(adr)]);
-			aa.add(gen, data, "skud", "ask","");
+			aa.add(scan, data, "skud", "ask","");
 		}
 	},
 	find(link){
@@ -242,27 +301,36 @@ let control={
 			}
 		})();		
 	},
+
 	skud(data, comment){
-		if(comment){
-			data=comment+" "+data+"\r\n";
-		}
-		pole.innerText=data+" "+(+new Date()-t_start);
+		pole.innerText=data+"\r\n"+(+new Date()-t_start);
 		t_start=+new Date();
+		console.log("skud -1 "+data[0]);
+		if(data[0] in controllers){
+			console.log("skud 1");
+			if(data[4]&0x1F){//ТМ
+				//по адресу находим объект контроллера
+				//в этот объект вставляем экземпляр генератора open_door
+				//если есть карта, добавляем ее номер в объект cards
+				console.log("new skud");
+				if(data[0] in controllers){
+					controllers[data[0]].open_door=open_door();
+					controllers[data[0]].open_door.next();
+				}
+			}else{
+				console.log("skud 2");
+				controllers[data[0]].open_door.next();
+			}
+			if(data[4]&0x10){//DOOR	
+			}
+		}
 	},
 	finder(data, comment){ //если ответ есть, добавляем адрес в массив
 		if(data[0]){
-			//controllers.add(data[0]);
 			if(!(data[0] in controllers)){
 				controllers[data[0]]={address:data[0]};
+				controllers[data[0]].open_door=return_null();//заглушка
 			}
-			//pole.innerText=data[0]+" "+(+new Date()-t_start);
-			//t_start=+new Date();
-			//let asd="";
-			//for (let value of controllers) {
-			//	asd=asd+" "+value;
-			//}
-			//pole.innerText="Addresses found: "+asd+"\r\n"+(+new Date()-t_start);
-			//t_start=+new Date();
 		}
 	},
 };
